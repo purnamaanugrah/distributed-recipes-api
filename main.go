97cc56248c43578ec7586871520e664fc128d3da
purnamaanugrah <purnamaanugrah98@gmail.com>
2023-01-30 03:17:14 +0800

    recipe data model

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type Recipe struct {
+	Name         string   `json:"name"`
+	Tags         []string `json:"tags"`
+	Ingredients  []string `json:"ingredients"`
+	Instructions []string `json:"instructions"`
+	PublishAt    string   `json:"publish_at"`
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
