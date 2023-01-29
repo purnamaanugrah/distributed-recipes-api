ff26289b2718c07d020fd15cf492bae650a86efb
purnamaanugrah <purnamaanugrah98@gmail.com>
2023-01-30 04:01:16 +0800

    update recipe endpoint

--- main.go
+++ main.go
@@ -36,6 +36,31 @@ func ListRecipeHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
 }
 
+func UpdateRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe Recipe
+	if err := c.ShouldBind(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+			break
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Recipe not found"})
+		return
+	}
+
+	recipes[index] = recipe
+	c.JSON(http.StatusOK, recipe)
+}
+
 func init() {
 	recipes = make([]Recipe, 0)
 	files, _ := ioutil.ReadFile("recipes.json")
